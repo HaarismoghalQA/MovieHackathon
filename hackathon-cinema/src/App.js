e5460328cd13c794a98735c715cdf1e643a65555
Andrew-McCall <amccall@qa.com>
2021-07-01 11:28:00 +0100

    AM: Cntact Us Form Created (No Bootstrap)

--- hackathon-cinema/src/App.js
+++ hackathon-cinema/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ContactUsPage from './components/contact';
 
 function App() {
   return (
--- hackathon-cinema/src/components/contact.jsx
+++ hackathon-cinema/src/components/contact.jsx
@@ -1,5 +1,26 @@
+// Bootstrap - Forms, Alert(Modal)
+import { useState } from 'react'
+
 const ContactUsPage = () => {
-    return ( <h1>Contact Us</h1> );
+
+    const [formData, setFormData] = useState({});
+
+    const handleSubmit = (e) => {
+         e.preventDefault();
+         alert('Thanks for Feedback.')
+       }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <label forHtml="name" > Name: </label><br/>
+            <input id="name" name="name" type="text" onChange={setFormData} required /> <br/>
+            <label forHtml="email" > Email: </label><br/>
+            <input id="email" name="email" type="email" onChange={setFormData} required /> <br/>
+            <label forHtml="message" > Message: </label><br/>
+            <textarea id="message" name="message" rows="4" cols="50" onChange={setFormData} required /> <br/>
+            <button type="submit"> Submit </button>
+        </form>
+     );
 }
  
 export default ContactUsPage;
