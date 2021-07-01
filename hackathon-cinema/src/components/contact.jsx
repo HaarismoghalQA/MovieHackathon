09c83acd9bd2d1de0eb2bd719fe0c0a564187374
Andrew-McCall <amccall@qa.com>
2021-07-01 11:41:29 +0100

    AM: Created UseState For Form Data

--- hackathon-cinema/src/components/contact.jsx
+++ hackathon-cinema/src/components/contact.jsx
@@ -9,15 +9,21 @@ const ContactUsPage = () => {
          e.preventDefault();
          alert('Thanks for Feedback.')
        }
-    
+
+    const handleChange = (e) => {
+        let newFormData = formData;
+        newFormData[e.target.name] = e.target.value;
+        setFormData(newFormData);
+    }
+
     return (
         <form onSubmit={handleSubmit}>
-            <label forHtml="name" > Name: </label><br/>
-            <input id="name" name="name" type="text" onChange={setFormData} required /> <br/>
-            <label forHtml="email" > Email: </label><br/>
-            <input id="email" name="email" type="email" onChange={setFormData} required /> <br/>
-            <label forHtml="message" > Message: </label><br/>
-            <textarea id="message" name="message" rows="4" cols="50" onChange={setFormData} required /> <br/>
+            <label forhtml="name" > Name: </label><br/>
+            <input id="name" name="name" type="text" onChange={handleChange} required /> <br/>
+            <label forhtml="email" > Email: </label><br/>
+            <input id="email" name="email" type="email" onChange={handleChange} required /> <br/>
+            <label forhtml="message" > Message: </label><br/>
+            <textarea id="message" name="message" rows="4" cols="50" onChange={handleChange} required /> <br/>
             <button type="submit"> Submit </button>
         </form>
      );
