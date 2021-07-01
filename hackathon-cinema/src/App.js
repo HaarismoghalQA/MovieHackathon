1f6982a69641ab6b6bed8a7a099f1972e2e55116
Andrew-McCall <amccall@qa.com>
2021-07-01 15:41:11 +0100

    AM: Landing Page

--- hackathon-cinema/src/App.js
+++ hackathon-cinema/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Aboutus from './components/Aboutus';
 import ContactUsPage from './components/contact';
 import { BrowserRouter, Route } from "react-router-dom";
 import Nav from './components/Navbar';
 import MyMovie from './components/MyMovie';
 import MovieFinder from './components/moviefinder';
+import LandingPage from './components/Landing';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Nav />
+
+        <Route path="/" exact>
+          <LandingPage/>
+        </Route>
 
         <Route path="/MyMovie">
           <MyMovie />
---
+++ hackathon-cinema/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+const LandingPage = () => {
+    return (
+    <main class="p-5 inner cover text-center">
+        <h1 class="cover-heading">Team "Byte Me!"</h1>
+        <p class="lead">This is our MERN Cinema Website. All our code is accessible on our GitHub. <br/>Group 2 Consists of: Andrew, Olvier, Haaris, Shameer</p>
+        <p class="lead">
+          <a href="https://github.com/HaarismoghalQA/MovieHackathon" class="btn btn-lg btn-secondary">GitHub</a>
+        </p>
+    </main>
+
+     );
+}
+ 
+export default LandingPage;
